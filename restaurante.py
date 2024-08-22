b711e526172d35a2e055f8cc56d06f5cbee90926
Vineogs <vineogs.2001@gmail.com>
2024-08-22 12:10:48 -0300

    Update restaurante.py

--- restaurante.py
+++ restaurante.py
@@ -2,17 +2,22 @@ class Restaurante:
     restaurantes = []
 
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
+        self._nome = nome
+        self._categoria = categoria
+        self._ativo = False
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self._categoria}'
 
     def listar_restaurantes():
+        print(f'{'Nome'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'.ljust(25)}')
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo.ljust(25)}')
+
+    @property
+    def ativo(self):
+        return '☑' if self._ativo else '☐'
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 restaurante_pizza = Restaurante('Pizza Express', 'Pizza')
